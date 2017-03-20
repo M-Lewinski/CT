5e3605d6541b73dde8bda39a2f0fc901423dceb9
lewinms <lewin.michal@gmail.com>
2017-03-20 09:20:17 +0100

    gui mk.2

--- main.py
+++ main.py
@@ -1,57 +1,79 @@
+import tkinter
+
 import matplotlib
+from reportlab.platypus.paraparser import sizeDelta
+
 matplotlib.use("TkAgg")
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
 import matplotlib.pyplot as plt
 import radon as rn
 import numpy as np
 from PIL import Image,ImageTk
 from skimage import data, color, measure
-from tkinter import *
+import tkinter as tk
 from tkinter import filedialog
-
+from tkinter import ttk
 
 step = np.pi/180
 detectorsNumber = 35
 detectorWidth = np.pi/6
 
-def main():
+def transformImage(graph):
     # path = filedialog.askopenfilename()
     inData = data.imread("input.png", as_grey=True)
-
-    plt.subplot(2, 2, 1)
-    plt.title("Original image")
-    plt.imshow(inData, cmap='gray')
-
-    plt.subplot(2, 2, 2)
-    plt.xlabel("Emiter/detector rotation")
-    plt.ylabel("Number of receiver")
-    plt.title("Radon transform image")
-
+    graph.changePlot((2,2,1),inData)
     radonImage = rn.radonTransform(inData)
-    plt.imshow(radonImage, cmap='gray')
-
-    # radonImage = rn.radonTransform(inData,[0])
-    # for i in range(1,180):
-    #    radonImage = rn.radonTransform(inData,[i],output=radonImage)
-    #    plt.imshow(radonImage,cmap='gray')
-
+    graph.changePlot((2, 2, 2), radonImage)
     inverseRadonImage = rn.inverseRadonTransform(radonImage)
-
-    plt.subplot(2, 2, 3)
-    plt.title("Inverse Radon transform image")
-    plt.imshow(inverseRadonImage, cmap='gray')
-
+    graph.changePlot((2, 2, 3), inverseRadonImage)
     filterImage = rn.filter(inverseRadonImage)
-    plt.subplot(2, 2, 4)
-    plt.title("Filtered image")
-    plt.imshow(filterImage, cmap='gray')
+    graph.changePlot((2, 2, 4), filterImage)
 
-    plt.show()
 
+class MainGui(tk.Tk):
+    def __init__(self):
+        tk.Tk.__init__(self)
+        tk.Tk.wm_title(self,"CT")
+        container = tk.Frame(self)
+        container.pack(side="top", fill="both", expand=True)
+        container.grid(row=2, column=1)
+        graph = Graph()
+        button = ttk.Button(container, text="Transform", command=lambda: transformImage(graph))
+        button.pack(pady=10, padx=10)
+        container.tkraise()
+        canvas = FigureCanvasTkAgg(graph, container)
+        graph.canvas = canvas
+        canvas.show()
+        canvas.get_tk_widget().pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
+
+
+class Graph(Figure):
+    plots = {}
+    canvas = None
+    def __init__(self, figsize=(10,10), dpi=100):
+        Figure.__init__(self, figsize=figsize, dpi=dpi)
+        titles = [{"title": "Original image"}, {"title": "Radon transform image", "xlabel": "Emiter/detector rotation", "ylabel": "Number of receiver"},
+                 {"title": "Inverse Radon transform image"}, {"title": "Filtered image"}]
+        for i in range(0, 4):
+            plt = self.add_subplot(2, 2, i+1)
+            if "title" in titles[i]:
+                plt.set_title(titles[i]["title"])
+            if "xlabel" in titles[i]:
+                plt.set_xlabel(titles[i]["xlabel"])
+            if "ylabel" in titles[i]:
+                plt.set_ylabel(titles[i]["ylabel"])
+            self.plots[(2, 2, i+1)] = plt
+
+
+    def changePlot(self,plot,image,cmap="gray"):
+        self.plots[plot].imshow(image,cmap=cmap)
+        self.canvas.show()
+
+
+def main():
+    app = MainGui()
+    app.mainloop()
 
 if __name__ == "__main__":
     main()
-
-def mainPanel():
-    global mainPanel,imagePanel
-    mainPanel
-    root = Tk()
